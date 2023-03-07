59539e7eec92c38850543c8f73f09915d8d93ad5
Lyxnx <a.julnes@gmail.com>
2023-03-07 20:46:05 +0000

    Add Coil to common libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ subprojects {
     apply(plugin = "maven-publish")
 
     group = "net.lyxnx.android"
-    version = "2023.03.06"
+    version = "2023.03.07"
 
     catalog {
         versionCatalog {
