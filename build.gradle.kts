3252e21710102135a25cc0fd5c330c5cbab844bc
Lyxnx <a.julnes@gmail.com>
2023-04-03 18:59:59 +0100

    Version 2023.04.03

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ subprojects {
     apply(plugin = "net.lyxnx.android.catalog-extensions")
 
     group = "net.lyxnx.android"
-    version = "2023.03.30"
+    version = "2023.04.03"
 
     catalog {
         versionCatalog {
