829516d5a3c7caa52455812e2df243b3a237c73c
Lyxnx <a.julnes@gmail.com>
2023-04-19 22:18:13 +0100

    Merge compose dependencies from bleeding into master (also includes the test library module updates)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,6 @@
 plugins {
     `version-catalog`
     id("net.lyxnx.android.catalog-extensions") version "1.0" apply false
-}
-
-subprojects {
-    apply(plugin = "version-catalog")
-    apply(plugin = "net.lyxnx.android.catalog-extensions")
-
-    group = "net.lyxnx.android"
-    version = "2023.04.05"
-
-    catalog {
-        versionCatalog {
-            from(files("libs.versions.toml"))
-        }
-    }
-
+    alias(common.plugins.android.library) apply false
+    alias(common.plugins.kotlin.android) apply false
 }
