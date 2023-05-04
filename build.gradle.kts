052d2eeb295879dc282109608800a5ce2cd0d6f4
Lyxnx <a.julnes@gmail.com>
2023-05-04 22:12:14 +0100

    Update test module

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     `version-catalog`
     id("net.lyxnx.android.catalog-extensions") version "1.0" apply false
-    alias(common.plugins.android.library) apply false
+    alias(common.plugins.android.application) apply false
     alias(common.plugins.kotlin.android) apply false
 }
