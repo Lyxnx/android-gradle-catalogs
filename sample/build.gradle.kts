322bbb83bbca3cd952d47a99c26fa16bf18fbd65
Lyxnx <a.julnes@gmail.com>
2023-08-30 22:48:27 +0100

    Remove sample app

--- sample/build.gradle.kts
+++
@@ -1,8 +0,0 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    alias(common.plugins.android.application) apply false
-    alias(common.plugins.kotlin.android) apply false
-
-    alias(common.plugins.ksp) apply false
-    id("io.github.lyxnx.android-compose-config") version "2023.08.29" apply false
-}
