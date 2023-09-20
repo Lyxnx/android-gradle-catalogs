6f3cbdc135c0d78f4b5684fe1e909b4eb4e00ec8
Lyxnx <a.julnes@gmail.com>
2023-09-20 23:29:05 +0100

    Version 2023.09.20

--- prerelease.sh
+++ prerelease.sh
@@ -16,6 +16,7 @@ properties_file="$current_dir/gradle.properties"
 external_file="$current_dir/versions-external/libs.versions.toml"
 version="$1"
 
-# Update the value in the properties file
 sed -i -E "s/CATALOGS_VERSION=.+/CATALOGS_VERSION=$version/g" "$properties_file"
 sed -i -E "s/catalogs-plugins = \".+\"/catalogs-plugins = \"$version\"/g" "$external_file"
+
+doctoc CHANGELOG.md --github
