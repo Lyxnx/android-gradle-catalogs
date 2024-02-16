d122497423aa4b5949662c566fef238572730ef1
Lyxnx <a.julnes@gmail.com>
2024-02-16 18:13:43 +0000

    Update publish catalogs task to publish the settings plugin in sync

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ tasks.register("publishCatalogs") {
     group = "Publishing"
     description = "Publishes all catalogs to Maven Central"
 
-    subprojects.filter { it.name.startsWith("versions-") }.forEach {
+    subprojects.filter { it.name.startsWith("versions-") || it.name == "settings-plugin" }.forEach {
         dependsOn(it.tasks["publish"])
     }
 }
